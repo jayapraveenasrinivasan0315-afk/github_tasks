bd89ae5c5fe2aaf06a0d15be17af1d9f46edcffd
Jaya Praveena S <jayapraveenas@genworx.ai>
2026-04-22 11:37:44 +0530

    changed wd

--- modules/artifact-repo/main.tf
+++ modules/artifact-repo/main.tf
@@ -1,9 +1,9 @@
 resource "google_artifact_registry_repository" "docker_repo" {
+  project       = var.project_id
   location      = var.region
   repository_id = var.repository_id
   description   = var.description
   format        = "DOCKER"
-  project_id    = var.project_id
 
   labels = var.labels
 }
