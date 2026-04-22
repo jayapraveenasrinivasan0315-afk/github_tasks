9c8a55bf6ee8cc683590432424582414446eaac4
Jaya Praveena S <jayapraveenas@genworx.ai>
2026-04-22 11:14:58 +0530

    format chnge

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 module "artifact_repo" {
-  source = "./modules/artifact-repo"
+  source        = "./modules/artifact-repo"
   repository_id = var.repository_id
-  description = var.description
-  labels = var.labels
-  project_id = var.project_id
-  region = var.region
+  description   = var.description
+  labels        = var.labels
+  project_id    = var.project_id
+  region        = var.region
 }
 
 module "gcs" {
-  source = "./modules/gcs"
-  bucket_name = var.bucket_name
-  region = var.region
-  force_destroy = var.force_destroy
-  enable_versioning = var.enable_versioning
+  source             = "./modules/gcs"
+  bucket_name        = var.bucket_name
+  region             = var.region
+  force_destroy      = var.force_destroy
+  enable_versioning  = var.enable_versioning
   enable_public_read = var.enable_public_read
-  labels = var.labels
+  labels             = var.labels
 }
 
 module "cloud_run" {
-  source = "./modules/cloud-run"
+  source       = "./modules/cloud-run"
   service_name = var.service_name
-  image = var.image
-  project_id = var.project_id
-  region = var.region
+  image        = var.image
+  project_id   = var.project_id
+  region       = var.region
 }
