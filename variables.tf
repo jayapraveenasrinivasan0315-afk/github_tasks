b04ad4b830271f5e5041f1e2415d800414ace48e
Jaya Praveena S <jayapraveenas@genworx.ai>
2026-04-22 11:42:50 +0530

    changed wd

--- variables.tf
+++ variables.tf
@@ -1,60 +1,60 @@
 variable "project_id" {
-    description = "The project ID to deploy resources"
-    type        = string
+  description = "The project ID to deploy resources"
+  type        = string
 }
 
 variable "region" {
-    description = "The region to deploy resources"
-    type        = string
+  description = "The region to deploy resources"
+  type        = string
 }
 
 variable "image" {
-    description = "The image to deploy"
-    type        = string
+  description = "The image to deploy"
+  type        = string
 }
 
 variable "bucket_name" {
-    description = "The name of the bucket"
-    type        = string
+  description = "The name of the bucket"
+  type        = string
 }
 
 variable "repository_id" {
-    description = "The Artifact Registry repository ID"
-    type        = string
+  description = "The Artifact Registry repository ID"
+  type        = string
 }
 
 variable "description" {
-    description = "Description for the Artifact Registry repository"
-    type        = string
+  description = "Description for the Artifact Registry repository"
+  type        = string
 }
 
 variable "labels" {
-    description = "Labels to apply to resources"
-    type        = map(string)
-    default = {
-        environment = "dev"
-    }
+  description = "Labels to apply to resources"
+  type        = map(string)
+  default = {
+    environment = "dev"
+  }
 }
 
 variable "service_name" {
-    description = "The name of the Cloud Run service"
-    type        = string
+  description = "The name of the Cloud Run service"
+  type        = string
 }
 
 variable "force_destroy" {
-    description = "Allow destruction of bucket with contents"
-    type        = bool
-    default     = true
+  description = "Allow destruction of bucket with contents"
+  type        = bool
+  default     = true
 }
 
 variable "enable_versioning" {
-    description = "Enable versioning on the bucket"
-    type        = bool
-    default     = true
+  description = "Enable versioning on the bucket"
+  type        = bool
+  default     = true
 }
 
 variable "enable_public_read" {
-    description = "Enable public read access to bucket"
-    type        = bool
-    default     = false
+  description = "Enable public read access to bucket"
+  type        = bool
+  default     = false
 }
