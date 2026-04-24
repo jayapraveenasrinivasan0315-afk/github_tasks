648a4ee6eefcada4a73ab220fa0c6388ec2d010b
Jaya Praveena S <jayapraveenas@genworx.ai>
2026-04-24 15:37:03 +0530

    del protection in cloud run

--- modules/cloud-run/main.tf
+++ modules/cloud-run/main.tf
@@ -2,6 +2,7 @@ resource "google_cloud_run_v2_service" "service" {
   name     = var.service_name
   location = var.region
   project  = var.project_id
+  deletion_protection = false
   
   template {
     containers {
